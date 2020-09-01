0076dc52bcc53b1e6786acbcd581d5fec5a8207f
Symphonic3 <hermennelson@gmail.com>
2020-09-01 12:32:40 -0600

    monieo

--- MonieoP2PClient/src/main/java/org/monieo/monieoclient/Monieo.java
+++ MonieoP2PClient/src/main/java/org/monieo/monieoclient/Monieo.java
@@ -3,7 +3,8 @@ package org.monieo.monieoclient;
 public class Monieo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("monieo");
 
 	}
 
