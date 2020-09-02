a36eebf35e5917ef3a8f3b11b6799764564e8be5
feves caler <johnreed1223@gmail.com>
2020-09-02 13:07:24 -0600

    added really cool key generation that is probably bad ☻☻♣◘

--- MonieoP2PClient/src/main/java/org/monieo/monieoclient/Monieo.java
+++ MonieoP2PClient/src/main/java/org/monieo/monieoclient/Monieo.java
@@ -1,9 +1,13 @@
 package org.monieo.monieoclient;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
 
 import javax.swing.JOptionPane;
@@ -20,87 +24,119 @@ import org.monieo.monieoclient.gui.MessageWithLink;
 import org.monieo.monieoclient.gui.UI;
 
 public class Monieo {
-	
+
 	public static UI ui;
 	public static double version;
-	
+	public static FileWriter writer = null;
+
 	public static void main(String[] args) {
-		
+
+		// rsa key pair generation and writing stuff
+		try {
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+			kpg.initialize(2048);
+			KeyPair kp = kpg.generateKeyPair();
+
+			File directory = new File(System.getenv("APPDATA") + "\\monieo");
+			if (!directory.exists()) {
+				new File(System.getenv("APPDATA") + "\\monieo").mkdir();
+				
+				File prvFile = new File(System.getenv("APPDATA") + "\\monieo\\public.txt");
+				prvFile.createNewFile();
+				writer = new FileWriter(prvFile);
+				writer.write(kp.getPublic().toString());
+				writer.close();
+				writer = null;
+				
+				File pubFile = new File(System.getenv("APPDATA") + "\\monieo\\private.txt");
+				pubFile.createNewFile();
+				writer = new FileWriter(pubFile);
+				writer.write(kp.getPrivate().toString());
+				writer.close();
+			}
+		} catch (NoSuchAlgorithmException | IOException e2) {
+			e2.printStackTrace();
+		}
+
 		final Properties properties = new Properties();
 		try {
 			properties.load(Monieo.class.getClassLoader().getResourceAsStream("project.properties"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Version: " + properties.getProperty("version"));
 		System.out.println("Please look towards the GUI application.");
 		System.out.println("");
 		System.out.println("If there are any issues with running the application, all errors will be logged in this window.");
 		System.out.println("Please paste the full log of this window when submitting a bug report.");
-		
-	    String url = "https://api.github.com/repos/Symphonic3/Monieo/releases/latest";
 
-	    CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(url);
-        request.addHeader("content-type", "application/json");
-        HttpResponse result = null;
+		String url = "https://api.github.com/repos/Symphonic3/Monieo/releases/latest";
+
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+		HttpGet request = new HttpGet(url);
+		request.addHeader("content-type", "application/json");
+		HttpResponse result = null;
 		try {
 			result = httpClient.execute(request);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-        if (result.getStatusLine().getStatusCode() != 200) throw new RuntimeException("Could not parse github API");
-        String json = null;
+		if (result.getStatusLine().getStatusCode() != 200)
+			throw new RuntimeException("Could not parse github API");
+		String json = null;
 		try {
 			json = EntityUtils.toString(result.getEntity(), "UTF-8");
 		} catch (ParseException | IOException e1) {
 			e1.printStackTrace();
 		}
 
-        JSONObject response = new JSONObject(json);
-        
-        double releaseLatest = Double.valueOf(response.getString("tag_name"));
-        version = Double.valueOf(properties.getProperty("version"));
-        
-        if (releaseLatest > version) {
-        	
-        	String link = "https://github.com/Symphonic3/Monieo/releases/tag/" + releaseLatest;
-        	
-        	int res = JOptionPane.showOptionDialog(null, new MessageWithLink("New update available. Press 'OK' to automatically download, or download and use version " + releaseLatest + " from github:"
-        			+ "\n <a href=\"" + link + "\">Click here</a>"), "New update available!", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null, null, null);
-        	
-        	if (res == 0) {
-        		
-        		JSONObject assetInfo = response.getJSONArray("assets").getJSONObject(0);
-        		String downloadURL = assetInfo.getString("browser_download_url");
-        		String fileName = assetInfo.getString("name");
-        		
-        		String jarFol = null;
+		JSONObject response = new JSONObject(json);
+
+		double releaseLatest = Double.valueOf(response.getString("tag_name"));
+		version = Double.valueOf(properties.getProperty("version"));
+
+		if (releaseLatest > version) {
+
+			String link = "https://github.com/Symphonic3/Monieo/releases/tag/" + releaseLatest;
+
+			int res = JOptionPane.showOptionDialog(null,
+					new MessageWithLink(
+							"New update available. Press 'OK' to automatically download, or download and use version "
+									+ releaseLatest + " from github:" + "\n <a href=\"" + link + "\">Click here</a>"),
+					"New update available!", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null, null, null);
+
+			if (res == 0) {
+
+				JSONObject assetInfo = response.getJSONArray("assets").getJSONObject(0);
+				String downloadURL = assetInfo.getString("browser_download_url");
+				String fileName = assetInfo.getString("name");
+
+				String jarFol = null;
 				try {
-					jarFol = new File(Monieo.class.getProtectionDomain().getCodeSource().getLocation()
-						    .toURI()).getParent();
+					jarFol = new File(Monieo.class.getProtectionDomain().getCodeSource().getLocation().toURI())
+							.getParent();
 				} catch (URISyntaxException e) {
 					e.printStackTrace();
 				}
-        		
-        		File newJar = new File(jarFol + "/" + fileName);
-        		
-        		try {
+
+				File newJar = new File(jarFol + "/" + fileName);
+
+				try {
 					FileUtils.copyURLToFile(new URL(downloadURL), newJar);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-        		
-        	}
-        	
-        	System.exit(0);
-        	
-        }
-	    
+
+			}
+
+			System.exit(0);
+
+		}
+
 		ui = new UI();
 		ui.initialize();
-		
+
 	}
 
-}	
+}
